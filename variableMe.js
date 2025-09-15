e9eda4e82c03834e6b090e93b8c13b26b0038921
Aryan Chettri <aryanchettri17@gmail.com>
2025-09-16 03:29:55 +0530

    This commit is about typescript and certain variables

---
+++ variableMe.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello Hitesh";
+greetings.toLowerCase();
+console.log(greetings);
---
+++ variableMe.ts
@@ -0,0 +1,24 @@
+let greetings: string = "Hello Hitesh";
+
+greetings.toLowerCase()
+
+console.log(greetings);
+
+
+//number
+
+let userId : number = 33333
+
+//boolean 
+
+let isLoggedIn : boolean = false
+
+//any
+
+let hero;
+
+function getHero(){
+    
+}
+
+export {}
